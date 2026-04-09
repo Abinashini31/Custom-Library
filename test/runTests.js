7dc980d8fa1b94dba0cbc4c18d76c1f0dbec23fe
Sandhya417 <sanss89.krish@gmail.com>
2026-04-09 22:00:44 +0530

    Changes made

--- test/runTests.js
+++
@@ -1,7 +0,0 @@
-import { runPluckTests } from './pluck-test.js';
-// Later: import { runLensTests } from './lens.test.js';
-
-console.log('Running Pluck Tests...');
-runPluckTests();
-
-// Later: runLensTests();
